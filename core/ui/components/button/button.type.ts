708dbb4c2ba282c2f893b233b6f8955c2415830f
Cebrail KILINÇ <87018423+Cebrailkilinc@users.noreply.github.com>
2024-07-10 22:27:46 +0300

    feat:update button

--- core/ui/components/button/button.type.ts
+++ core/ui/components/button/button.type.ts
@@ -3,6 +3,7 @@ import { buttonVariants } from './button.cva';
 import * as React from 'react';
 
 // ButtonProps arayüzü
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, Omit<VariantProps<typeof buttonVariants>, 'disabled'> {
+export interface ButtonProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'color'>, Omit<VariantProps<typeof buttonVariants>, 'disabled' | 'color'> {
   disabled?: boolean;
+  color?: 'primary' | 'secondary' | 'thirty';
 }
